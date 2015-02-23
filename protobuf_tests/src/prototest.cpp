aa65bc47621b1214ed464e6416c3bab306352061
Gauthier Zirnhelt <gauthier.zirnhelt@ensimag.imag.fr>
2015-02-23 12:29:27 +0100

    Prototest

--- protobuf_tests/src/prototest.cpp
+++ protobuf_tests/src/prototest.cpp
@@ -1,5 +1,24 @@
+#include <string>
+#include <iostream>
+#include "proto/entity.pb.h"
 
 int main(void)
 {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    test::Component comp;
+    comp.set_type(test::Component::COMP_0);
+    std::string str("TEST");
+    comp.add_data(str);
+
+    std::cout << comp.DebugString();
+    std::string serial;
+    comp.SerializeToString(&serial);
+    
+    test::Component comp2;
+    comp2.ParseFromString(serial);
+    comp2.DebugString();
+
+    google::protobuf::ShutdownProtobufLibrary();
     return 0;
 }
